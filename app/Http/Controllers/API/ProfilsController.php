e4bc6d3c26296b8451404eb0d3b3db5f74cd56e4
MartineauAn <martineau.antoine2001@gmail.com>
2021-01-11 12:02:34 +0100

    :tada: profil update

--- app/Http/Controllers/API/ProfilsController.php
+++ app/Http/Controllers/API/ProfilsController.php
@@ -7,6 +7,7 @@ use App\Models\Employee;
 use App\Models\Sector;
 use App\Models\SectorDistrict;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProfilsController extends Controller
 {
@@ -64,9 +65,39 @@ class ProfilsController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $int11 = "99999999999";
+
         $employee = Employee::find($id);
 
-        return $employee->update($request->all());
+        $validator = Validator::make($request->all(), [
+            'login'=>'required|string|max:45',
+            'code'=>'required|string|max:5',
+            'sectorDistrict_id'=>'required|numeric|max:'.$int11,
+            'leader_id'=>'numeric|max:'.$int11,
+            'postalCode'=>'required|string|max:5',
+            'firstname'=>'required|string|max:255',
+            'lastname'=>'required|string|max:255',
+            'address'=>'required|string|max:255',
+            'city'=>'required|string|max:255',
+            'phone'=>'required|string|max:10',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['message'=>'error']);
+        }
+
+        $employee->login = $request->input('login');
+        $employee->code = $request->input('code');
+        $employee->leader_id = $request->input('leader_id');
+        $employee->sectorDistrict_id = $request->input('sectorDistrict_id');
+        $employee->postalCode = $request->input('postalCode');
+        $employee->firstname = $request->input('firstname');
+        $employee->lastname = $request->input('lastname');
+        $employee->address = $request->input('address');
+        $employee->city = $request->input('city');
+        $employee->phone = $request->input('phone');
+
+        return response()->json($employee->save());
 
     }
 
