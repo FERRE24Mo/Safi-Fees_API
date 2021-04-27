44728e743453b8f1f6f8dcc288e57254da14b4cc
Killian P <KillianPiccerelle@sfr.fr>
2021-04-27 09:52:18 +0200

    commit

--- app/Http/Controllers/API/SheetsController.php
+++ app/Http/Controllers/API/SheetsController.php
@@ -20,7 +20,7 @@ class SheetsController extends Controller
      */
     public function index()
     {
-        $sheets = Expense::all();
+        $sheets = ExpenseSheet::all();
 
         return response()->json($sheets);
 
@@ -32,9 +32,15 @@ class SheetsController extends Controller
         $date = $date::now()->format('Y-m');
 
 
-        $currentSheet = Expense::where('creationDate', 'like', $date . '-%')->where('employee_id', $employee_id)->get();
+        $currentSheet = ExpenseSheet::where('creationDate', 'like', $date . '-%')->where('employee_id', $employee_id)->get();
 
-        return response()->json([$currentSheet]);
+        foreach ($currentSheet[0]->expenses as $expense){
+
+            Expense::getAllInformations($expense);
+
+        }
+
+        return response()->json([$currentSheet[0]]);
     }
 
     public function last($employee_id){
@@ -42,32 +48,39 @@ class SheetsController extends Controller
         $date = new Date();
         $date = $date::now()->sub('1 month')->format('Y-m');
 
-        $currentSheet = Expense::where('creationDate', 'like', $date . '-%')->where('employee_id', $employee_id)->get();
+        $currentSheet = ExpenseSheet::where('creationDate', 'like', $date . '-%')->where('employee_id', $employee_id)->get();
 
-        return response()->json([$currentSheet]);
+
+        foreach ($currentSheet[0]->expenses as $expense){
+
+            Expense::getAllInformations($expense);
+
+        }
+
+        return response()->json([$currentSheet[0]]);
     }
 
-    public function historical($employee_id){
+   /* public function historical($employee_id){
 
-        $sheets = Expense::where('expenseSheetState_id','<>','1')->where('employee_id', $employee_id)->get();
+        $sheets = ExpenseSheet::where('expenseSheetState_id','<>','1')->where('employee_id', $employee_id)->get();
 
         return response()->json([$sheets]);
-    }
+    }*/
 
     public function validated($employee_id){
-        $sheets = Expense::where('expenseSheetState_id','5')->orWhere('expenseSheetState_id','3')->where('employee_id', $employee_id)->get();
+        $sheets = ExpenseSheet::where('expenseSheetState_id','5')->orWhere('expenseSheetState_id','3')->where('employee_id', $employee_id)->get();
 
         return response()->json([$sheets]);
     }
 
     public function unvalidated($employee_id){
-        $sheets = Expense::where('expenseSheetState_id','4')->where('employee_id', $employee_id)->get();
+        $sheets = ExpenseSheet::where('expenseSheetState_id','2')->where('employee_id', $employee_id)->get();
 
         return response()->json([$sheets]);
     }
 
     public function inWaitingAndError($employee_id){
-        $sheets = Expense::where('expenseSheetState_id','6')->orWhere('expenseSheetState_id','2')->where('employee_id', $employee_id)->get();
+        $sheets = ExpenseSheet::where('expenseSheetState_id','6')->orWhere('expenseSheetState_id','4')->where('employee_id', $employee_id)->get();
 
         return response()->json([$sheets]);
     }
@@ -135,7 +148,7 @@ class SheetsController extends Controller
      */
     public function destroy($sheet_id)
     {
-        $currentSheet = Expense::find($sheet_id);
+        $currentSheet = ExpenseSheet::find($sheet_id);
 
         return response()->json(['Status'=>$currentSheet->delete()]);
     }
--- routes/api.php
+++ routes/api.php
@@ -58,9 +58,9 @@ Route::middleware('jwt.auth')->group(function (){
      */
     Route::get('sheets/{employee_id}/current',[\App\Http\Controllers\API\SheetsController::class,'current']);
     Route::get('sheets/{employee_id}/last',[\App\Http\Controllers\API\SheetsController::class,'last']);
-    Route::get('sheets/user/{employee_id}/historical',[\App\Http\Controllers\API\SheetsController::class,'historical']);
-    Route::get('sheets/user/{employee_id}/validated',[\App\Http\Controllers\API\SheetsController::class,'validated']);
+    /*Route::get('sheets/user/{employee_id}/historical',[\App\Http\Controllers\API\SheetsController::class,'historical']);*/
     Route::get('sheets/user/{employee_id}/unvalidated',[\App\Http\Controllers\API\SheetsController::class,'unvalidated']);
+    Route::get('sheets/user/{employee_id}/validated',[\App\Http\Controllers\API\SheetsController::class,'validated']);
     Route::get('sheets/user/{employee_id}/inWaitingAndError',[\App\Http\Controllers\API\SheetsController::class,'inWaitingAndError']);
 
     Route::get('sheet/{sheet_id}',[\App\Http\Controllers\API\SheetsController::class,'show']);
